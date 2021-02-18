108934b6109b6027f46f553082e8e6871c7dca37
Taq Karim <taq.karim@oracle.com>
2021-02-18 04:53:33 -0500

    add todos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,15 +5,15 @@ import com.kpandas.series.Series
 fun zero(i: Int) = 5
 
 fun main() {
-//    val s = Series<String, Any>(mutableListOf("a", "b", null, "c"), name ="foo")
-//    println(s)
-//    var t = s * 5
-//    println(t)
+    // create new NDArray
     val m = NDArray<Int>()
     println(m.values)
+    // TODO: remove this, ndarrays are immutable
     m.add(5)
     println((m + DType(15)).values)
     println(m.values)
+    // create series objects from ndarrays
+    // TODO: take a mutableList and convert to NDArray behind the scenes
     val s = Series<Any, Any>(NDArray(mutableListOf(1, 2, null, 3)), NDArray(mutableListOf("a", "b", "c", "d")), name ="foo")
     println(s)
     var t = s + 5
